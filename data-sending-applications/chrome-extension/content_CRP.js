ccdb527f1f9b99ebf8379e6455b76e3b6a73aff9
perite <nikita.krementar@gmail.com>
2024-12-15 21:57:51 +0100

    add classes to content_CRP.js for better longtime support
    changed logo

--- data-sending-applications/chrome-extension/content_CRP.js
+++ data-sending-applications/chrome-extension/content_CRP.js
@@ -1,107 +1,137 @@
-const findTitleName = () => {
-    return document.getElementsByClassName('romanji')[0].textContent
-};
+class Activity {
+    constructor(name) {
+        this.oldData = "";
+        this.name = name
+    }
 
-const getCurrentEpisode = () => {
-    const playlistContainer = document.getElementsByClassName("playlists-videos")[0];
-    const listItems = playlistContainer.getElementsByTagName("ul")[0];
-    const listArrayRaw = Array.from(listItems.children);
-    const listArray = listArrayRaw.filter(item => item.getAttribute("data-file").startsWith("https://iv.sibnet.ru/"));
-
-    let current_episode = -1;
-    for (let i = 0; i < listArray.length; i++) {
-        if (listArray[i].classList.contains("visible") && listArray[i].classList.contains("active")) {
-            current_episode = i + 1;
-            return current_episode;
+    sendData = (info, method) => {
+        const dataToSend = {
+            action: "sendData",
+            info: info,
+            method: method,
+            activity: this.name
         }
-    }
-};
-const getMALTitleId = () => {
-    const links = document.getElementsByClassName('ansdb');
-    for (let link of links) {
-        if (link.href.startsWith("https://myanimelist.net/anime/")) {
-            return link.href.split("/")[4]
+
+        if (JSON.stringify(this.oldData) === JSON.stringify(dataToSend)) {
+            console.log("Data was not sent, identical to old")
+            return
         }
-    }
-};
 
+        chrome.runtime.sendMessage(dataToSend)
+        console.log("Sent data to background.js | Info :", info, "| Method :", method, "| Activity : ", this.name)
+        this.oldData = dataToSend
 
-const getInfoFromPage = () => {
-    return {
-        "title_name": titleName,
-        "current_episode": getCurrentEpisode(),
-        "mal_title_id": MALTitleID
-    }
-};
+    };
 
-const sendData = (info, method, activity) => {
-    const dataToSend = {
-        action: "sendData",
-        info: info,
-        method: method,
-        activity: activity
-    }
-    console.log(dataToSend, dataOld)
-    if (JSON.stringify(dataOld) === JSON.stringify(dataToSend)) {
-        console.log("Data was not sent, identical to old")
-        return
+    startSending = () => {
+        console.error("startSending was not implemented")
     }
 
-    chrome.runtime.sendMessage(dataToSend)
-    console.log("Sent data to background.js | Info :", info, "| Method :", method, "| Activity_DONT_NEED ", activity)
-    dataOld = dataToSend
-
-};
-
-const sendDataUpdate = () => {
-    console.log("uopdate")
-    sendData(getInfoFromPage(), "update", "WatchingAnimeJoy");
+    stopSending = () => {
+        console.error("stopSending was not implemented")
+    }
 }
 
-const sendDataClear = () => {
-    console.log("before")
-    sendData(getInfoFromPage(), "clear", "WatchingAnimeJoy");
+class WatchingAnimeJoy extends Activity {
+    constructor() {
+        super("WatchingAnimeJoy")
+        this.titleName = this.findTitleName();
+        this.MALTitleID = this.getMALTitleId();
 
-}
+        this.updateIntervalId = NaN;
+
+    }
+
+    get currentEpisode() {
+        const playlistContainer = document.getElementsByClassName("playlists-videos")[0];
+        const listItems = playlistContainer.getElementsByTagName("ul")[0];
+        const listArrayRaw = Array.from(listItems.children);
+        const listArray = listArrayRaw.filter(item => item.getAttribute("data-file").startsWith("https://iv.sibnet.ru/"));
+
+        let current_episode = -1;
+        for (let i = 0; i < listArray.length; i++) {
+            if (listArray[i].classList.contains("visible") && listArray[i].classList.contains("active")) {
+                current_episode = i + 1;
+                return current_episode;
+            }
+        }
+    }
+
+    get titleInfo() {
+        return {
+            "title_name": this.titleName,
+            "current_episode": this.currentEpisode,
+            "mal_title_id": this.MALTitleID
+        };
+    }
+
+    findTitleName = () => {
+        return document.getElementsByClassName('romanji')[0].textContent
+    };
+
+    getMALTitleId = () => {
+        const links = document.getElementsByClassName('ansdb');
+        for (let link of links) {
+            if (link.href.startsWith("https://myanimelist.net/anime/")) {
+                return link.href.split("/")[4]
+            }
+        }
+    };
 
 
-console.log('Start sending 1th time')
-
-let titleName = "";
-let MALTitleID = "";
-setTimeout(() => {
-    titleName = findTitleName();
-    MALTitleID = getMALTitleId();
-}, 500)
-
-
-let dataOld = "";
-
-
-let updateIntervalId = setInterval(() => {
-    sendDataUpdate();
-}, 5000);
-
-console.log("Created update interval with id : ", updateIntervalId)
-
-document.addEventListener('visibilitychange', () => {
-    if (document.visibilityState === 'visible') {
-        console.log('Start sending')
-        sendDataUpdate();
-
-        updateIntervalId = setInterval(() => {
-            sendDataUpdate()
+    startSending = () => {
+        this.updateIntervalId = setInterval(() => {
+            this.sendData(this.titleInfo, "update");
         }, 5000);
-        console.log("Created update interval with id :", updateIntervalId)
-
-    } else {
-        console.log("Need to clear |", "updateIntervalId :", updateIntervalId)
-        clearInterval(updateIntervalId);
-        sendDataClear();
     }
-});
 
-window.addEventListener('beforeunload', () => {
-    console.log("before")
-    sendDataClear();
+    stopSending = () => {
+        this.sendData(this.titleInfo, "clear");
+        clearInterval(this.updateIntervalId);
+    }
+}
+
+
+class Geoguessr extends Activity {
+    constructor() {
+        super("Geoguessr");
+    }
+
+    startSending = () => {
+        this.sendData({"status": "started"}, "update")
+    }
+
+    stopSending = () => {
+        this.sendData({"status": "finished"}, "clear")
+    }
+}
+
+window.addEventListener("load", () => {
+    const hostName = (window.location.hostname).split(".").reverse()[1]
+    let act;
+    switch (hostName) {
+
+        case "animejoy":
+            act = new WatchingAnimeJoy();
+            break;
+        case 'geoguessr':
+            act = new Geoguessr();
+            break;
+
+        default:
+            return
+    }
+
+    act.startSending();
+
+    document.addEventListener('visibilitychange', () => {
+        if (document.visibilityState === 'visible') {
+            act.startSending()
+
+        } else {
+            act.stopSending()
+        }
+    });
+
+
 })
